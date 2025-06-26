ef9dc4eab3de408574fd46b5132792fadc78652b
darkweb-alt <pitambaryadav2061@gmail.com>
2025-06-26 18:10:57 +0530

    OHH WHTAIS THIS

--- Week1/variables.java
+++ Week1/variables.java
@@ -2,6 +2,6 @@ package Week1;
 
 public class variables {
     public static void main(String []args){
-        System.out.println("Nothing here to sow ");
+        System.out.println("Nothing here to show ");
     }
 }
---
+++ typecasting.java
@@ -0,0 +1,5 @@
+class typecasting{
+    public static void main(String[] args){
+        System.out.println("Hello Pitambar, I am here to learn the week one of GFG course");
+    }
+}
