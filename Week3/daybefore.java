570ed65b0c93f1b2deb63c4a1df80bcb94e67dd1
darkweb-alt <pitambaryadav.2401266@srec.ac.in>
2025-09-20 21:40:33 +0530

    Added days before N

--- Week3/daybefore.java
+++ Week3/daybefore.java
@@ -1,5 +1,25 @@
 package Week3;
 
-public class daybefore {
-    
+import java.util.Scanner;
+
+public class daybefore{
+    public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+String[] days={"Sunday","Monday","Tuesday","Wednesday",
+"Thursday","Friday","Saturday"};
+
+System.out.println("Enter current day");
+int c=sc.nextInt();
+System.out.println("Enter day before to find");
+int b=sc.nextInt();
+int before=b%7;
+if(c-before>0){
+System.out.println(days[c-before]);
+
+}
+else{
+    System.out.println(days[c-before+7]);
+}
+
+    }
 }
