9f2a057ecdd0fa71d5f3b221b7e58716579c0958
darkweb-alt <pitambaryadav.2401266@srec.ac.in>
2025-09-20 21:11:57 +0530

    Adding

--- Week2/output.java
+++ Week2/output.java
@@ -1,3 +1,5 @@
+package Week2;
+
 class output{
     public static void main(String[] args) {
         int a=5;
---
+++ Week3/lastdigit.java
@@ -0,0 +1,9 @@
+package Week3;
+
+public class lastdigit {
+    public static void main(String[] args){
+        int n=-5;
+        System.out.println(Math.abs(n));
+        System.out.println(n);
+    }
+}
