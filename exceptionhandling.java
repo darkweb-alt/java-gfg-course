23fd128580f8b6da04ce465d7351194b75176508
darkweb-alt <pitambaryadav.2401266@srec.ac.in>
2025-10-03 16:18:51 +0530

    updating exceptionhandling

---
+++ exceptionhandling.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class exceptionhandling{
+    public static void main(String[] args){
+        try{
+            Scanner sc=new Scanner(new File("a.txt"));//checked exception:
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Exception handling "+e.getMessage());
+        
+        }
+        System.out.println("Hello Pitambar, I can print our name, seeeee");
+    }
+}
