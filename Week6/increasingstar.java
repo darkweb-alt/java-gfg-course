057be2ed9155155afab39d4785f7087539829635
darkweb-alt <pitambaryadav.2401266@srec.ac.in>
2025-12-10 19:30:07 +0530

    added some codes

---
+++ Week6/increasingstar.java
@@ -0,0 +1,27 @@
+ package Week6;
+
+public class increasingstar {
+
+    public static void main(String[] args) {
+        // int i=1;
+        // while(i<=5){
+        //     int j=1;
+        //     while(j<=5){
+        //         System.out.println("Hello ");
+        //         j++;
+        //     }
+        //     i++;
+        // }
+for(int i=3;i>=0;i--){
+    for(int j=i;j<3;j++){
+        System.out.print("*");
+    }
+    System.out.println();
+}
+
+
+
+
+
+    }
+}
