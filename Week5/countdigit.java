2f18bbbc805ab22e83a742c8b37303b799e0d3ea
darkweb-alt <pitambaryadav.2401266@srec.ac.in>
2025-12-10 18:43:41 +0530

    added folder for next week

--- Week5/countdigit.java
+++ Week5/countdigit.java
@@ -1,12 +1,20 @@
 package Week5;
 
+import java.util.Scanner;
+
 public class countdigit {
     public static void main(String[] args) {
         int number=12345;
         int count=0;
-        while(number/10 !=0){
+        Scanner sc=new Scanner(System.in);
+        int a;
+        System.out.println("Enter number to check the number of digits: ");
+        a=sc.nextInt();
+
+        while(a>0){
+            a=a/10;
             count=count+1;
         }
-        System.out.println("The number of erms are: "+count);
+        System.out.println("The number of terms are: "+count);
     }
 }
